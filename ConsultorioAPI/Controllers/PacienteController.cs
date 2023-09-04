37a96a1351dbb30179094de6e3eb3e6424d40368
Helena M Luz <helena1703h@gmail.com>
2023-09-04 16:11:43 -0300

    Criando o service do Paciente

--- ConsultorioAPI/Controllers/PacienteController.cs
+++ ConsultorioAPI/Controllers/PacienteController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ConsultorioAPI.DTOs;
+using ConsultorioAPI.Models;
+using ConsultorioAPI.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ConsultorioAPI.Controllers
@@ -7,5 +10,66 @@ namespace ConsultorioAPI.Controllers
     [ApiController]
     public class PacienteController : ControllerBase
     {
+
+        private readonly IPacienteService _service;
+        public PacienteController(IPacienteService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Paciente>>> GetAllPacientes()
+        {
+            return Ok(await _service.GetAllPacientes());
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Paciente>> GetPacienteById(int Id)
+        {
+            return Ok(await _service.GetPacienteById(Id));
+        }
+
+        [HttpGet("consulta/{Id}")]
+        public async Task<ActionResult<List<Consulta>>> GetPacienteConsultaById(int Id)
+        {
+           return Ok(await  _service.GetPacienteConsultaById(Id));
+        }
+
+        [HttpPut("{Id}")]
+        public async Task<ActionResult> UpdatePaciente(PacienteDTO paciente, int Id)
+        {
+    
+            await _service.UpdatePaciente(paciente, Id);
+            return Ok(paciente.Nome + " atualizado com sucesso!");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreatePaciente(PacienteDTO paciente)
+        {
+            if (paciente == null) return BadRequest("Preencha os campos!");
+            await _service.CreatePaciente(paciente);
+            return Ok(paciente.Nome + " criado com sucesso!");
+        }
+
+        [HttpGet("idade/{Idade}")]
+        public async Task<ActionResult<List<Paciente>>> GetPacienteByIdade(int Idade)
+        {
+            if (Idade <= 0) return BadRequest("Idade precisa ser maior que 0!");
+            return Ok(await _service.GetPacienteByIdade(Idade));
+        }
+
+        [HttpGet("sangue/{tipo}")]
+        public async Task<ActionResult<List<Paciente>>> GetPacienteBySangue(string tipo)
+        {
+            return Ok(await _service.GetPacienteByTipoSanguineo(tipo));
+        }
+
+        [HttpPatch("{Id}")]
+        public async Task<ActionResult> UpdateEnderecoPaciente(string endereco, int Id)
+        {
+           
+            await _service.UpdateEnderecoPaciente(endereco, Id);
+            return Ok(" Paciente atualizado com sucesso!");
+        }
     }
 }
--- ConsultorioAPI/Service/Interfaces/IPacienteService.cs
+++ ConsultorioAPI/Service/Interfaces/IPacienteService.cs
@@ -1,6 +1,17 @@
-﻿namespace ConsultorioAPI.Service.Interfaces
+﻿using ConsultorioAPI.DTOs;
+using ConsultorioAPI.Models;
+
+namespace ConsultorioAPI.Service.Interfaces
 {
     public interface IPacienteService
     {
+        Task<List<Paciente>> GetAllPacientes();
+        Task<Paciente> GetPacienteById(int Id);
+        Task<List<Consulta>> GetPacienteConsultaById(int id);
+        Task UpdatePaciente(PacienteDTO paciente, int Id);
+        Task CreatePaciente(PacienteDTO paciente);
+        Task<List<Paciente>> GetPacienteByIdade(int Idade);
+        Task<List<Paciente>> GetPacienteByTipoSanguineo(string tipo);
+        Task UpdateEnderecoPaciente(string endereco, int Id);
     }
 }
